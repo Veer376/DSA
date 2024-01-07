2cae1425a91fcd9c1576f1830700789667a90e62
Veer376 <aryaveer.22gcebaiml050@galgotiacollege.edu>
2024-01-07 18:45:22 +0530

    TREE

---
+++ Trees.java
@@ -0,0 +1,29 @@
+public class Trees {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+                 }
+    }
+    static class BuildTree{
+       static int index=-1;
+       static Node buildTree(int[] nodes){
+           index++;
+           if(nodes[index]==-1) return null;
+           Node node= new Node(nodes[index]);
+           node.left=buildTree(nodes);
+           node.right=buildTree(nodes);
+           return node;
+       }
+    }
+    public static void main(String[] args) {
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        Node head=BuildTree.buildTree(nodes);
+        System.out.println(head.right.data);
+
+    }
+}
