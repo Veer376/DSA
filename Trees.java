5e4afb0e5f62c5ab82625a1df732ec953017a37f
Veer376 <aryaveer.22gcebaiml050@galgotiacollege.edu>
2024-01-07 18:51:30 +0530

    countNodes Function

--- Trees.java
+++ Trees.java
@@ -20,10 +20,17 @@ public class Trees {
            return node;
        }
     }
+
+    static int countNodes(Node head){
+        if(head==null) return 0;
+        int countleft=countNodes(head.left);
+        int countright=countNodes(head.right);
+        return countleft+countright+1;
+    }
     public static void main(String[] args) {
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         Node head=BuildTree.buildTree(nodes);
-        System.out.println(head.right.data);
+        System.out.println(countNodes(head));
 
     }
 }
