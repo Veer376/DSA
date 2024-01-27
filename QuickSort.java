4ec81a3b087f7a4333494a49ad17b23c924e9b30
Veer376 <aryaveer.22gcebaiml050@galgotiacollege.edu>
2024-01-27 19:54:54 +0530

    code updated

--- QuickSort.java
+++ QuickSort.java
@@ -1,26 +1,31 @@
 public class QuickSort {
-    static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    static void swap(int[] arr, int i, int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    static int partition(int[] arr, int start, int end){
+        int pivot=arr[end];
+        int i=start-1;
+        while(start<end){
+            if(arr[start]<pivot){
+                i++;
+                swap(arr,i,start);
+            }
+            start++;
+        }
+        swap(arr,i+1,end);
+        return i+1;
     }
     static void quicksort(int[] arr, int start, int end){
         if(start>=end) return;
-        int pivot=arr[end];
-        int pivot_index=start;
-        for(int i=start;i<end;i++){
-            if(arr[i]<pivot){
-                swap(arr,i,pivot_index);
-                pivot_index++;
-            }
-        }
-        swap(arr,pivot_index,end);
-        quicksort(arr,start,pivot_index-1);
-        quicksort(arr,pivot_index+1,end);
+        int pivot=partition(arr,start,end);
+        quicksort(arr,start,pivot-1);
+        quicksort(arr,pivot+1,end);
     }
 
     public static void main(String[] args) {
-        int[] arr={1,5,3,2,7,0};
+        int[] arr={1,5,3,4};
         quicksort(arr,0,arr.length-1);
         for(int i:arr){
             System.out.print(i+" ");
